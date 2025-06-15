3537ed834009876aef14e9d1b2c41ae75cc6bc0b
Ian Foster <iwf777@hotmail.com>
2025-06-14 21:59:26 -0500

    draw circle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,6 @@
 <script lang="ts">
-    import { Grid, Row, Column } from "carbon-components-svelte";
-    import "carbon-components-svelte/css/all.css"
-
-    let rows = 10;
-    let columns = 10;
+    import GameGrid from "$lib/GameGrid.svelte";
 </script>
 
 
-<!-- <style>
-    :global(.bx--col) {
-        border: 1px solid #ccc;
-        min-height: 40px;
-    }
-</style> -->
-
-<div class="mb-12"></div>
-<div>
-    <Grid class="game-grid">
-        {#each Array(rows) as _, row_idx}
-            <Row>
-                {#each Array(columns) as _, col_idx}
-                    <Column class="text-xs">({row_idx}, {col_idx})</Column>
-                {/each}
-            </Row>
-        {/each}
-    </Grid>
-</div>
+<GameGrid />

42e49b1cb373a26b7d63459ddb2dfb5adde6a283
Ian Foster <iwf777@hotmail.com>
2025-06-14 21:51:52 -0500

    a nice grid with carbon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Grid, Row, Column } from "carbon-components-svelte";
+    import "carbon-components-svelte/css/all.css"
+
+    let rows = 10;
+    let columns = 10;
+</script>
+
+
+<!-- <style>
+    :global(.bx--col) {
+        border: 1px solid #ccc;
+        min-height: 40px;
+    }
+</style> -->
+
+<div class="mb-12"></div>
+<div>
+    <Grid class="game-grid">
+        {#each Array(rows) as _, row_idx}
+            <Row>
+                {#each Array(columns) as _, col_idx}
+                    <Column class="text-xs">({row_idx}, {col_idx})</Column>
+                {/each}
+            </Row>
+        {/each}
+    </Grid>
+</div>
